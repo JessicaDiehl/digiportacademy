fac266b9550e29a591b8cfb52ec0ac988ab49568
I514493 <jessica.diehl@sap.com>
2024-06-19 12:04:00 -0300

    adicionando exercicios 3 e 4

--- revisao/revisao/exercicio1.go
+++ revisao/revisao/exercicio1.go
@@ -1,13 +1,33 @@
 package revisao
+
 import "fmt"
 
 func Funcao1() {
 
 	listaDeDespesas := []string{"mercado", "aluguel", "eletricidade", "faculdade"}
 
-	var i int
-
-	for i=0; i< len(listaDeDespesas); i++{
-		fmt.Printf("%s\n", listaDeDespesas[i])
+	fmt.Println("Lista de despesas:")
+	for _, despesa := range listaDeDespesas {
+		fmt.Println(despesa)
 	}
+
+	fmt.Println("\nVerificação de despesa específica:")
+	var despesaBuscada string =  "mercado"
+
+	fmt.Scanln(&despesaBuscada)
+
+	encontrouDespesa := false
+	for _, despesa := range listaDeDespesas {
+		if despesa == despesaBuscada {
+			encontrouDespesa = true
+			break
+		}
+	}
+	if encontrouDespesa {
+		fmt.Printf("%s está na lista de despesas.\n", despesaBuscada)
+	} else {
+		fmt.Printf("%s não está na lista de despesas.\n", despesaBuscada)
+	}
+
+	fmt.Printf("\nTotal de despesas na lista: %d\n", len(listaDeDespesas))
 }
